4652f140df363a99c7a3cf698fe8ed486de80041
ColdCipri <colder.ciprian@gmail.com>
2019-12-11 15:50:21 +0200

    Added .net core files

---
+++ LFTC-Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LFTC_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
